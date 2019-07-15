6089797fd0890e15f3f154b83a081af726aecdb5
thatch256 <thatch256@gmail.com>
2019-07-15 17:58:28 -0600

    Updating project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import './reset.css'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav className='navbar'>
+          <div className='nav-title'>
+            Start Bootstrap
+          </div>
+          <i className="fas fa-bars" />
+          <span className="menu">SERVICES PORTFOLIO ABOUT TEAM CONTACT</span>
+          
+        </nav>
       </header>
     </div>
   );
